740972d0fa0f9b550a45a254b15d1eefcf0b5cd6
noanmee42 <zhuchatskiy+42@gmail.com>
2026-02-03 23:23:17 +0800

    working extractor and hid an API key

--- Python/main.py
+++
@@ -1,8 +0,0 @@
-# Вариант 1
-from dotenv import load_dotenv
-load_dotenv()  # Загружает .env
-
-from claim_extractor import ClaimExtractor
-
-extractor = ClaimExtractor()  # Теперь найдет ключ
-claims = extractor.extract("Япония - большой остров в Тихом океане с населением 125 миллионов человек, основанным в 660 году до н.э.")
